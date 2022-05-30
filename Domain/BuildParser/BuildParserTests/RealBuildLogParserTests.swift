65ce93cf3f00ef820855b0dc96ab3a8eec76fd81
Mikhail Rubanov <m.rubanov@dodopizza.com>
2022-05-30 10:35:37 +0500

    Remove performance test from Filter

--- Domain/BuildParser/BuildParserTests/RealBuildLogParserTests.swift
+++ Domain/BuildParser/BuildParserTests/RealBuildLogParserTests.swift
@@ -19,7 +19,7 @@ class RealBuildLogParserTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func test_whenParseShouldFindPathToDependency() throws {
         let parser = RealBuildLogParser()
         
         let testBundle = TestBundle()
